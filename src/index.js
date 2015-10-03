9a21dc6839d46aac85b1d2e1f7f293083e20ee6b
Francis Brito <fr.br94@gmail.com>
2015-10-03 17:22:35 -0400

    Update app factory to return a Koa application.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /**
  * External imports
  */
+import koa from 'koa';
 import libdebug from 'debug';
 
 // create debug logger
@@ -8,4 +9,6 @@ const debug = libdebug('lgho:root');
 
 export default function create(options = {}) {
   debug('options %j', options);
+
+  return koa();
 }
