40872098f2bdf79507f3d5cc52c7db1c26b1d5b6
Francis Brito <fr.br94@gmail.com>
2015-10-03 17:25:58 -0400

    Update root path to return status code 200.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@
  * External imports
  */
 import koa from 'koa';
+import route from 'koa-route';
 import libdebug from 'debug';
 
 // create debug logger
 const debug = libdebug('lgho:root');
 
+/**
+ * Routes
+ */
+function* root() {
+  this.response.status = 200;
+}
+
 export default function create(options = {}) {
   debug('options %j', options);
 
-  return koa();
+  return koa()
+  .use(route.get('/', root));
 }
