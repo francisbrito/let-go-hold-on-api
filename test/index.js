20adcf2d1c43df8ffa27dfcbe67e6443adadc353
Francis Brito <fr.br94@gmail.com>
2015-10-12 12:18:57 -0400

    Add root route content and content type tests.

--- test/index.js
+++ test/index.js
@@ -2,13 +2,9 @@
 
 import co from 'co';
 import test from 'blue-tape';
-import agent from 'promisify-supertest';
+import createTestAgent from 'promisify-supertest';
 
 import createApplication from '../src';
-
-const setup = () => {
-  return agent(createApplication().callback());
-};
 
 test('GET /', (sub) => {
   sub.test('responds with OK status code', co.wrap(function* (assert) {
@@ -25,4 +21,47 @@ test('GET /', (sub) => {
       assert.equal(statusCode, okStatusCode, 'should be status OK');
     }
   }));
+
+  sub.test('responds with JSON content type', co.wrap(function* (assert) {
+    const fixture = setup();
+
+    yield fixture
+    .get('/')
+    .expect(contentTypeToBeJson)
+    .end();
+
+    function contentTypeToBeJson({type}) {
+      const expectedType = 'application/json';
+
+      assert.equal(type, expectedType, 'should be application/json');
+    }
+  }));
+  sub.test('responds with an object with `hold on` and `let go` keys',
+  co.wrap(function* (assert) {
+    const fixture = setup();
+
+    yield fixture
+    .get('/')
+    .expect(bodyToBeTruthy)
+    .expect(bodyToHaveKeys('hold on', 'let go'))
+    .end();
+
+    function bodyToBeTruthy({body}) {
+      assert.ok(body, 'should be truthy');
+    }
+
+    function bodyToHaveKeys(...keys) {
+      return function bodyToHaveKeysAssertion({body}) {
+        keys.forEach((key) => {
+          assert.ok(key in body, `should have key ${key}`);
+        });
+      };
+    }
+  }));
 });
+
+function setup() {
+  const app = createApplication();
+
+  return createTestAgent(app.callback());
+}
