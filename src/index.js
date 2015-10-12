d8aac8b081f5df7c7c331d78d25e25958cfe5676
Francis Brito <fr.br94@gmail.com>
2015-10-12 12:24:09 -0400

    Update root route to respond with hold-on/let-go object.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 /**
  * External imports
  */
-import koa from 'koa';
 import route from 'koa-route';
-import libdebug from 'debug';
+import createDebugLogger from 'debug';
+import createKoaApplication from 'koa';
 
 // create debug logger
-const debug = libdebug('lgho:root');
+const log = createDebugLogger('lgho:root');
 
 /**
  * Routes
  */
 function* root() {
+  this.response.body = {
+    'let go': null,
+    'hold on': null,
+  };
   this.response.status = 200;
 }
 
 export default function create(options = {}) {
-  debug('options %j', options);
+  log('options %j', options);
 
-  return koa()
+  return createKoaApplication()
   .use(route.get('/', root));
 }
